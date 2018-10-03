c7ea81df4c33ef8df6f4a1eebb34fa627081f76e
Hugo <arnaud.hugo@orange.fr>
2018-10-03 10:59:20 +0200

    ajout du fichier data.json

--- diabeteJava/src/diabeteJava/Critere.java
+++ diabeteJava/src/diabeteJava/Critere.java
@@ -1,21 +1,19 @@
 package diabeteJava;
 
-import java.util.ArrayList;
-
 public class Critere {
-	private String Age;
-	private String TourdtH; /*Tour de taille Homme*/
-	private String TourdtF; /*Tour de taille Femme*/
+	private int Age;
+	private int TourdtH; /*Tour de taille Homme*/
+	private int TourdtF; /*Tour de taille Femme*/
 	private String ActPhy;
 	private String ATCDTraitAntiHTA; /*Antecedent traitement anti-HTA*/
 	private String ATCDFamDiabete; /*Antecedent famillial de diabete*/
 	private String ATCDGlycePlusNorm; /*Antecedent glycemie superieur à la normale*/
-	private String IMC; /* Indice masse corporel*/
+	private int IMC; /* Indice masse corporel*/
 	private String PartLegumeVert;
 	
 	
-	public Critere(String age, String tourdtH, String tourdtF, String actPhy, String aTCDTraitAntiHTA, String aTCDFamDiabete,
-		String aTCDGlycePlusNorm, String iMC, String partLegumeVert) {
+	public Critere(int age, int tourdtH, int tourdtF, String actPhy, String aTCDTraitAntiHTA, String aTCDFamDiabete,
+			String aTCDGlycePlusNorm, int iMC, String partLegumeVert) {
 		Age = age;
 		TourdtH = tourdtH;
 		TourdtF = tourdtF;
@@ -26,76 +24,92 @@ public class Critere {
 		IMC = iMC;
 		PartLegumeVert = partLegumeVert;
 	}
-	
-	
-    
-	public String getAge() {
+
+
+	public int getAge() {
 		return Age;
 	}
 
-	public void setAge(String age) {
+
+	public void setAge(int age) {
 		Age = age;
 	}
 
-	public String getTourdtH() {
+
+	public int getTourdtH() {
 		return TourdtH;
 	}
 
-	public void setTourdtH(String tourdtH) {
+
+	public void setTourdtH(int tourdtH) {
 		TourdtH = tourdtH;
 	}
 
-	public String getTourdtF() {
+
+	public int getTourdtF() {
 		return TourdtF;
 	}
 
-	public void setTourdtF(String tourdtF) {
+
+	public void setTourdtF(int tourdtF) {
 		TourdtF = tourdtF;
 	}
+
 
 	public String getActPhy() {
 		return ActPhy;
 	}
 
+
 	public void setActPhy(String actPhy) {
 		ActPhy = actPhy;
 	}
+
 
 	public String getATCDTraitAntiHTA() {
 		return ATCDTraitAntiHTA;
 	}
 
+
 	public void setATCDTraitAntiHTA(String aTCDTraitAntiHTA) {
 		ATCDTraitAntiHTA = aTCDTraitAntiHTA;
 	}
+
 
 	public String getATCDFamDiabete() {
 		return ATCDFamDiabete;
 	}
 
+
 	public void setATCDFamDiabete(String aTCDFamDiabete) {
 		ATCDFamDiabete = aTCDFamDiabete;
 	}
+
 
 	public String getATCDGlycePlusNorm() {
 		return ATCDGlycePlusNorm;
 	}
 
+
 	public void setATCDGlycePlusNorm(String aTCDGlycePlusNorm) {
 		ATCDGlycePlusNorm = aTCDGlycePlusNorm;
 	}
 
-	public String getIMC() {
+
+	public int getIMC() {
 		return IMC;
 	}
 
-	public void setIMC(String iMC) {
+
+	public void setIMC(int iMC) {
 		IMC = iMC;
 	}
+
 
 	public String getPartLegumeVert() {
 		return PartLegumeVert;
 	}
+
 
 	public void setPartLegumeVert(String partLegumeVert) {
 		PartLegumeVert = partLegumeVert;
@@ -103,38 +117,33 @@ public class Critere {
 	
 	public String AgeIndividu(String reponse) {
 
-		String R1 = "<45";
-		String R2 = "45-54";
-		String R3 = "55-64";
-		String R4 = ">64";
-		reponse = "<45";
-		if (reponse.compareTo(R1) == 0 ) {
+        String R1 = "<45";
+        String R2 = "45-54";
+        String R3 = "55-64";
+        String R4 = ">64";
+        reponse = "<45";
+        if (reponse.compareTo(R1) == 0 ) {
 
-			this.Age = R1;
-		}
-		
-		if (reponse.compareTo(R2) == 0) {
+            this.Age = R1;
+        }
+        
+        if (reponse.compareTo(R2) == 0) {
 
-			this.Age = R2;
-		}
-		
-		if (reponse.compareTo(R3) == 0) {
+            this.Age = R2;
+        }
+        
+        if (reponse.compareTo(R3) == 0) {
 
-			this.Age = R3;
-		}
-					
-		if (reponse.compareTo(R4) == 0) {
+            this.Age = R3;
+        }
+                    
+        if (reponse.compareTo(R4) == 0) {
 
-			this.Age = R4;
-		}
-		return this.Age;				
-	}
+            this.Age = R4;
+        }
+        System.out.println(this.Age);
+        return this.Age;                
+    }
 
-	public int tourdt(String reponse) {
-		//if (Individu.getSexe() == Homme)
-			
-"			alors la c'est push"
-			
-			
-	}
+    
 }
